e438fcf2e8ed057113a37793ac9691da41d9f0fd
Germain Boue <boue.germain@gmail.com>
2016-04-11 21:13:21 -0700

    Add USER_DATA and two Components for Profile Link and Profile Pic

--- app/index.js
+++ app/index.js
@@ -1,2 +1,36 @@
-var app = document.getElementById('app');
-app.innerHTML = 'hello!';
+var USER_DATA = {
+  name: 'Germain Boue',
+  username: 'gboue',
+  image: 'https://avatars2.githubusercontent.com/u/5375979?v=3&s=460'
+
+}
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var ProfilePic = React.createClass({
+  render: function(){
+    return (
+      return <img src="{this.props.imageUrl" style={{height: 100, width: 100}}/>
+    )
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    )
+  }
+});
+
+
+
+ReactDOM.render(
+  <HelloWorld name="Germain"/>,
+  document.getElementById('app')
+)
