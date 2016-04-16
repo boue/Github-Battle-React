598f143da180f8128694faa53b7b42967c66f3ff
Germain Boue <boue.germain@gmail.com>
2016-04-16 16:14:15 -0700

    Add some animations between routes

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,11 +1,19 @@
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+require('../main.css'); //webpack enabled 
 
 //Main needs to take responsibility for rendering child component
+//need child el to have key, clone element and add attribute key
 var Main = React.createClass({
   render: function(){
     return (
       <div className='main-container'>
-        {this.props.children}
+        <ReactCSSTransitionGroup
+          transitionName="appear"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}>
+            {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+        </ReactCSSTransitionGroup>
       </div>
     )
   }
