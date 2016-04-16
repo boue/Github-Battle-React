9a88cf3dae06d900e48675066781a5b2c46441eb
Germain Boue <boue.germain@gmail.com>
2016-04-15 20:09:16 -0700

    Build UI for main battle page buit still need some templating

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -23,10 +23,20 @@ var ConfirmBattleContainer = React.createClass({
         })
       }.bind(this))
   },
+  //react-router lets you push props to new routes (data we got from promises)
+  handleInitiateBattle: function() {
+    this.context.router.push({
+      pathname: '/results',
+      state: {
+        playersInfo: this.state.playersInfo
+      }
+    })
+  },
   render: function () {
     return (
       <ConfirmBattle
         isLoading={this.state.isLoading}
+        onInitiateBattle={this.handleInitiateBattle}
         playersInfo={this.state.playersInfo} />
     )
   }
